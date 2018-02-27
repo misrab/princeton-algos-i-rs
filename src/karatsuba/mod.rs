4f95a779500842ff0485e4a0683fcd419f157586
misrab <faizullah.misrab@gmail.com>
2018-02-27 17:46:43 +0800

    added string karatsuba method. getting better with rust, but still getting weird overflow errors. might save it for the next assignment or project

--- src/karatsuba/mod.rs
+++ src/karatsuba/mod.rs
@@ -15,9 +15,9 @@
 */
 
 
-//use std::u64::pow;
 
 
+use std::cmp;
 
 
 fn slice_to_int(x: &[u64]) -> u64 {
@@ -26,7 +26,7 @@ fn slice_to_int(x: &[u64]) -> u64 {
   let n = x.len();
 
   for power in 0..n {
-    result += x[power] * 10u64.pow(n as u64 - 1 -power as u64);
+    result += x[power] * 10u64.pow(n as u32 - 1 - power as u32);
   }
 
   result
@@ -34,7 +34,15 @@ fn slice_to_int(x: &[u64]) -> u64 {
 
 
 fn int_to_vec(num: u64) -> Vec<u64> {
-  num.to_string().chars().map(|d as u64| d.to_digit(10).unwrap()).collect()
+  num.to_string().chars().map(|d| d.to_digit(10).unwrap() as u64).collect()
+}
+
+
+
+// helper function to play around with manually
+// in code without reading from command line
+fn string_to_vec(s: &str) -> Vec<u64> {
+  s.chars().map(|d| d.to_digit(10).unwrap() as u64).collect()
 }
 
 
@@ -46,7 +54,8 @@ fn int_to_vec(num: u64) -> Vec<u64> {
 fn multiply(x: &[u64], y: &[u64]) -> u64 {
 
   let n = x.len();
-  let n32: u64 = x.len() as u64;
+
+  let n32: u32 = x.len() as u32;
   //let n_y = y.len();
 
 
@@ -73,6 +82,10 @@ fn multiply(x: &[u64], y: &[u64]) -> u64 {
 
 
   let step3 = multiply(&int_to_vec(slice_to_int(a)+slice_to_int(b)), &int_to_vec(slice_to_int(c)+slice_to_int(d)));
+
+
+  println!("doing {:?} minus {:?} minus {:?}", step3, bd, ac);
+
   let ad_plus_bc = step3 - bd - ac;
 
 
@@ -83,21 +96,121 @@ fn multiply(x: &[u64], y: &[u64]) -> u64 {
 }
 
 
-// helper function to play around with manually
-// in code without reading from command line
-fn string_to_vec(s: &str) -> Vec<u64> {
-  s.chars().map(|d as u64| d.to_digit(10).unwrap()).collect()
+
+// trying out a string version instead
+fn multiply_string(x: &str, y: &str) -> u64 {
+  let n_x = x.len();
+  let n_y = y.len();
+  let n = cmp::max(n_x, n_y);
+  let n2 = n/2; // ! this should be a floor
+
+
+  // base case
+  if n_x == 1 || n_y == 1 {
+    return parse_int(x) * parse_int(y);
+  }
+
+  // get the pieces
+  let a = &x[0..n_x - n2];
+  let b = &x[n_x - n2..n_x];
+  let c = &y[0..n_y - n2];
+  let d = &y[n_y - n2..n_y];
+
+  let a_plus_b = parse_int(a) + parse_int(b);
+  let c_plus_d = parse_int(c) + parse_int(d);
+
+  //let a_plus_b_string = a_plus_b.to_string();
+  //let c_plus_d
+
+
+  let ac = multiply_string(a, c);
+  let step3 = multiply_string(&a_plus_b.to_string(), &c_plus_d.to_string());
+  let bd = multiply_string(b, d);
+
+  let ad_plus_bc = step3 - ac - bd;
+
+  // ! consider adding trailing zeros to string instead
+  let term1 = parse_int(&add_trailing_zeros(&ac.to_string(), n2 as u64 * 2));
+  let term2 = parse_int(&add_trailing_zeros(&ad_plus_bc.to_string(), n2 as u64));
+  let term3 = bd;
+
+  term1 + term2 + term3
+}
+
+fn add_trailing_zeros(string: &str, numzeros: u64) -> String {
+  let mut owned_string: String = string.to_owned();
+  for i in 0..numzeros {
+    owned_string.push_str("0");
+  }
+
+  owned_string
+}
+
+fn parse_int(input: &str) -> u64 {
+  input.chars()
+    .find(|a| a.is_digit(10))
+    .and_then(|a| a.to_digit(10))
+    .unwrap() as u64
+}
+
+#[test]
+fn test_add_trailing_zeros() {
+  let x = "101";
+  let x_big = &add_trailing_zeros(x, 3);
+  assert_eq!(x_big, "101000");
 }
 
 
+#[test]
+fn test_karatsuba_string() {
+  let x = "5678";
+  let y = "1234";
+  let result = multiply_string(x, y);
+  assert_eq!(result, 7006652, "{:?} times {:?} equals {:?}.", x, y, result);
+}
+
+
+#[test]
+#[ignore]
+fn test_karatsuba() {
+  println!("testing karatsuba!");
+
+
+
+
+  let x = &[5,6,7,8];
+  let y = &[1,2,3,4];
+  let result = multiply(x, y);
+
+  // answer should be 7006652;
+  assert_eq!(result, 7006652, "{:?} times {:?} equals {:?}.", x, y, result);
+
+  return;
+
+  //let p_str = "3141592653589793238462643383279502884197169399375105820974944592";
+  //let q_str = "2718281828459045235360287471352662497757247093699959574966967627";
+  let p_str = "3141592653589";//79323846264338";//3279502884197169399375105820974944592";
+  let q_str = "2718281828459";//04523536028747";//1352662497757247093699959574966967627";
+  let p = &string_to_vec(p_str);
+  let q = &string_to_vec(q_str);
+
+
+
+  let pqresult = multiply(p,q);
+  println!("pqresult {:?}", pqresult);
+}
 
 #[test]
 fn test_int_to_vec() {
   let slice = &vec![3,4,5];
   let int = 345;
   let convert = &int_to_vec(int);
-
   assert_eq!(convert, slice, "slice {:?} and int {:?}", slice, int);
+
+  let slice2 = &vec![4];
+  let int2 = 4;
+  let convert2 = &int_to_vec(int2);
+  assert_eq!(convert2, slice2, "slice {:?} and int {:?}", slice2, int2);
 }
 
 
@@ -109,24 +222,16 @@ fn test_slice_to_int() {
 
   assert_eq!(convert, int, "slice {:?} and int {:?}", slice, int);
 
+  let slice2 = &vec![4];
+  let int2 = 4;
+  let convert2 = slice_to_int(slice2);
+  assert_eq!(convert2, int2, "slice {:?} and int {:?}", slice2, int2);
+
+  let slice3 = &vec![0,0,7];
+  let int3 = 7;
+  let convert3 = slice_to_int(slice3);
+  assert_eq!(convert3, int3, "slice {:?} and int {:?}", slice3, int3);
+
+
 }
 
-#[test]
-fn test_karatsuba() {
-  println!("testing karatsuba!");
-
-  let x = &[5,6,7,8];
-  let y = &[1,2,3,4];
-  let result = multiply(x, y);
-
-  // answer should be 7006652;
-  assert_eq!(result, 7006652, "{:?} times {:?} equals {:?}.", x, y, result);
-
-
-  let p_str = "3141592653589793238462643383279502884197169399375105820974944592";
-  let q_str = "2718281828459045235360287471352662497757247093699959574966967627";
-  let p = &string_to_vec(p_str);
-  let q = &string_to_vec(q_str);
-  let pqresult = multiply(p,q);
-  println!("pqresult {:?}", pqresult);
-}
