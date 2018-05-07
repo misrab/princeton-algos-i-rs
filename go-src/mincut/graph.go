921aeb9af29c2bc816175c8cd6a4c61a61086b91
Misrab <faizullah.misrab@gmail.com>
2018-05-07 10:26:05 +0800

    basic node addition

--- go-src/mincut/graph.go
+++ go-src/mincut/graph.go
@@ -1,15 +1,25 @@
 package graph
 
-import ()
+import (
+	"fmt"
+)
 
 type Vertex struct {
 	id          uint64
 	connections []*Edge
 }
 
+func (v Vertex) String() string {
+	return fmt.Sprintf("{\n\tid: %d\n\tconnections: %v\n}", v.id, v.connections)
+}
+
 type Edge struct {
 	from *Vertex
 	to   *Vertex
+}
+
+func (e *Edge) String() string {
+	return fmt.Sprintf("(%v,%v)", e.from.id, e.to.id)
 }
 
 type graph struct {
@@ -22,9 +32,9 @@ type Graph interface {
 
 	insertNodeAdjacency(id uint64, connections []uint64)
 
-	getNodeAdjacency(id uint64) []*Edge
+	//GetNodeAdjacency(id uint64) []*Edge
 
-	getNode(uint64) (*Vertex, bool)
+	GetNode(uint64) (*Vertex, bool)
 }
 
 func NewGraph() Graph {
@@ -32,12 +42,12 @@ func NewGraph() Graph {
 
 	// allocate memory
 	graph.vertices = make(map[uint64]*Vertex)
-	//graph.edges = make([]*Edge)
+	graph.edges = make([]*Edge, 0)
 
 	return graph
 }
 
-func (g *graph) getNode(id uint64) (*Vertex, bool) {
+func (g *graph) GetNode(id uint64) (*Vertex, bool) {
 	v, ok := g.vertices[id]
 	return v, ok
 	/*for i := 0; i < len(g.vertices); i++ {
@@ -50,49 +60,42 @@ func (g *graph) getNode(id uint64) (*Vertex, bool) {
 	*/
 }
 
-func (g *graph) getNodeAdjacency(id uint64) []*Edge {
-	node, ok := g.getNode(id)
-
-	if !ok {
-		return nil
-	}
-	return node.connections
-}
-
-//func (g *graph) insertNode(id uint64) {
-//}
-
 func (g *graph) insertNodeAdjacency(id uint64, connections []uint64) {
-	num_connections := len(connections)
+	//num_connections := len(connections)
 
 	// if the node is in the graph, get it
 	// otherwise create a new one
-	node, ok := g.getNode(id)
+	node, ok := g.GetNode(id)
 	if !ok {
 		node = &Vertex{
 			id:          id,
-			connections: make([]*Edge, num_connections),
+			connections: make([]*Edge, 0),
 		}
 	}
 
-	// TODO add connections
+	// add connections
 	// makes sure all nodes exist in the graph
 	for _, v := range connections {
-		n, ok := g.getNode(v)
+		new_vertex, ok := g.GetNode(v)
 		if !ok {
 			// create the node and add it
-			g.vertices[v] = &Vertex{
-				id:          v,
-				connections: make([]*Edge, 0),
-			}
+			new_vertex = new(Vertex)
+			new_vertex.id = v
+			new_vertex.connections = make([]*Edge, 0)
+			g.vertices[v] = new_vertex
 		}
 
-		new_edge := &Edge{
-			from: node,
-			to:   n,
-		}
+		new_edge := new(Edge)
+		new_edge.from = node
+		new_edge.to = new_vertex
 
+		//fmt.Printf("adding edge %v to connections of %v: %v\n", new_edge, id, node.connections)
+
+		// add to both nodes
 		node.connections = append(node.connections, new_edge)
+		new_vertex.connections = append(new_vertex.connections, new_edge)
+		// add to graph
+		g.edges = append(g.edges, new_edge)
 	}
 
 	g.vertices[id] = node
--- go-src/mincut/graph_test.go
+++ go-src/mincut/graph_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"log"
 	"testing"
 )
 
@@ -9,4 +10,11 @@ func TestNewGraph(t *testing.T) {
 
 	// insert nodes
 	graph.insertNodeAdjacency(1, []uint64{2, 3, 4, 5})
+
+	node1, _ := graph.GetNode(1)
+	log.Printf("%v\n", node1)
+
+	node2, _ := graph.GetNode(2)
+	log.Printf("%v\n", node2)
+
 }
